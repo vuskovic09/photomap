a5a4c6b4c2ecfc1bf264d890ff693efe71103ed3
Filip Vušković <filip.vuskovic09@gmail.com>
2020-09-20 01:59:56 +0200

    ADD: map API + gulp4 migration

--- gulpfile.js
+++ gulpfile.js
@@ -15,42 +15,43 @@ config = {
 }
 
 // sass
-gulp.task('scss', function () {
+gulp.task('scss', gulp.series(function () {
     return gulp.src(config.resources + 'sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gcmq())
         .pipe(cssnano())
         .pipe(autoprefixer())
         .pipe(gulp.dest(config.destination + 'css'))
-});
+}));
 
 // js
-gulp.task('js', function () {
+gulp.task('js', gulp.series(function () {
     return gulp.src([config.resources + 'js/vendor/*.js', config.resources + 'js/*.js'])
         .pipe(concat('scripts.js'))
         .pipe(uglify())
         .pipe(gulp.dest(config.destination + 'js'));
-});
+}));
 
 // images
-gulp.task('images', function () {
+gulp.task('images', gulp.series(function () {
     return gulp.src(config.resources + 'images/**/*')
         .pipe(gulp.dest(config.destination + 'images'))
-});
+}));
 
 // fonts
-gulp.task('fonts', function () {
+gulp.task('fonts', gulp.series(function () {
     return gulp.src(config.resources + 'fonts/**/*')
         .pipe(gulp.dest(config.destination + 'fonts'))
-});
+}));
 
 // default
-gulp.task('default', ['js', 'scss', 'images', 'fonts']);
+gulp.task('default', gulp.series(['js', 'scss', 'images', 'fonts']));
 
 // watch
-gulp.task('watch', function () {
-    gulp.watch('resources/sass/**/*.scss', ['scss']);
-    gulp.watch('resources/js/**/*.js', ['js']);
-    gulp.watch('resources/images/**/*', ['images']);
-    gulp.watch('resources/fonts/**/*', ['fonts']);
-});
+gulp.task('watch', gulp.series(function () {
+    gulp.watch('resources/sass/**/*.scss',  gulp.series(['scss'])),
+    gulp.watch('resources/js/**/*.js',  gulp.series(['js'])),
+    gulp.watch('resources/images/**/*',  gulp.series(['images'])),
+    gulp.watch('resources/fonts/**/*',  gulp.series(['fonts']));
+    return;
+}));
--- resources/js/script.js
+++ resources/js/script.js
@@ -3,7 +3,7 @@
 */
 
 $(document).ready(function() {
-
+  initMap();
 
 });
 
@@ -16,3 +16,14 @@ $(window).resize(function() {
 
 
 });
+
+function initMap() {
+  var options = {
+    zoom: 3,
+    center: {lat: 33.5477919, lng: 40.3364947}
+  }
+
+  var map = new google.maps.Map(document.getElemenstByClassName('map'), {
+
+  })
+}
